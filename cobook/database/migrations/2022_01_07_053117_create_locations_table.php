0e671cee46fbe562d9cbcf5b5319a334fa577acf
thonixon <thonixon321@gmail.com>
2022-01-06 22:53:24 -0800

    issue #2 - setting up seeders and migrations

--- cobook/database/migrations/2022_01_07_053117_create_locations_table.php
+++ cobook/database/migrations/2022_01_07_053117_create_locations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateLocationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('user_id');
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('location_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('address');
+            $table->string('latitude');
+            $table->string('longitude');
             $table->timestamps();
-            $table->unsignedTinyInteger('userEn')->default(1);
+            $table->unsignedTinyInteger('locationEn')->default(1);
         });
     }
 
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('locations');
     }
 }
